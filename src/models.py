3bf724fa72ea31729b680fe48cb0f2bdecec297b
LMezza <mezabalza18@gmail.com>
2025-02-12 01:39:50 +0000

    diagrama

--- src/models.py
+++ src/models.py
@@ -1,29 +1,51 @@
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.orm import relationship, declarative_base, Mapped, mapped_column
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class Users(Base):
+    __tablename__ = 'users'
+    # Here we define columns for the table users
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(String(80), nullable=False, unique=True)
+    favs_planets: Mapped["Favs_planets"] = relationship(back_populates="users")
+    favs_characters: Mapped["Favs_characters"] = relationship(back_populates="users")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Characters_planets(Base):
+    __tablename__ = 'characters_planets'
+    # Here we define columns for the table characters & planets
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    type: Mapped[str] = mapped_column(nullable=False)
+    favs_planets: Mapped["Favs_planets"] = relationship(back_populates="characters_planets")
+    favs_planets: Mapped["Favs_characters"] = relationship(back_populates="characters_planets")
+
+class Favs_planets(Base):
+    __tablename__ = 'favs_planets'
+    # Here we define columns for the table favs_planets.
+    # Notice that each column is also a normal Python instance attribute.
+    id: Mapped[int] = mapped_column(primary_key=True)
+    users_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    users: Mapped["Users"] = relationship(back_populates="favs_planets")
+    charaters_planets_id: Mapped[int] = mapped_column(ForeignKey("characters_planets.id"))
+    characters_planets: Mapped["Characters_planets"] = relationship(back_populates="favs_planets")
+
+class Favs_characters(Base):
+    __tablename__ = 'favs_characters'
+    # Here we define columns for the table favs_characters.
+    # Notice that each column is also a normal Python instance attribute.
+    id: Mapped[int] = mapped_column(primary_key=True)
+    users_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    users: Mapped["Users"] = relationship(back_populates="favs_characters")
+    charaters_planets_id: Mapped[int] = mapped_column(ForeignKey("characters_planets.id"))
+    characters_planets: Mapped["Characters_planets"] = relationship(back_populates="favs_characters")
 
     def to_dict(self):
         return {}
